1f707fa6b03ffefbf2c3ba8722c9be75167679e4
ensobosna <147910441+EnsoBIH@users.noreply.github.com>
2024-11-11 13:43:46 +0100

    add java Binaere-Suche

--- java/src/App.java
+++ java/src/App.java
@@ -12,16 +12,35 @@ public class App {
 
         reader.close();
 
-        System.out.println(search(number));
+        System.out.println(lineare_serach(number));
+        System.out.println(binary_search(number, 0, Numbers.length - 1));
     }
 
-    private static String search(int number) {
+    private static String lineare_serach(int value) {
         for (int i = 0; i < Numbers.length; i++) {
-            if (Numbers[i] == number) {
-                return Numbers[i] + " is the number you are looking for";
+            if (Numbers[i] == value) {
+                return Numbers[i] + " is the number you are looking for and it is at index " + i;
             }
         }
 
         return "Number not found";
+    }
+
+    private static String binary_search(int value, int start, int end) {
+        int middle = (start + end) / 2;
+        
+        if (start > end) {
+            return "Number not found";
+        }
+
+        if (value == Numbers[middle]) {
+            return Numbers[middle] + " is the number you are looking for and it is at index " + middle;
+        }
+
+        if (value < Numbers[middle]) {
+            return binary_search(value, start, middle - 1);
+        } else {
+            return binary_search(value, middle + 1, end);
+        }
     }
 }
